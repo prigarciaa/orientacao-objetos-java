7b405693b9faa0270591447b8e655b111a542756
prigarciaa <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>
2025-07-16 18:25:29 -0300

    Métodos retorno

---
+++ src/academy/prigarcia/orientacaoobjetos/javacore/introducaometodos/test/CalculadoraTest03.java
@@ -0,0 +1,11 @@
+package academy.prigarcia.orientacaoobjetos.javacore.introducaometodos.test;
+
+import academy.prigarcia.orientacaoobjetos.javacore.introducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        double result = calculadora.divideDoisNumeros(20, 2);
+        System.out.println(result);
+    }
+}

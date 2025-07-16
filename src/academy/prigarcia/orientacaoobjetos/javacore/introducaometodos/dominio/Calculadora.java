7a6a7027a49c0017afbde90c4369c0158a534600
prigarciaa <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>
2025-07-16 18:07:01 -0300

    Métodos parâmetros

--- src/academy/prigarcia/orientacaoobjetos/javacore/introducaometodos/dominio/Calculadora.java
+++ src/academy/prigarcia/orientacaoobjetos/javacore/introducaometodos/dominio/Calculadora.java
@@ -9,4 +9,7 @@ public class Calculadora {
         System.out.println(21 - 2);
 
     }
+    public void multiplicaDoisNumeros(int num1, int num2) {
+        System.out.println(num1 * num2);
+    }
 }
---
+++ src/academy/prigarcia/orientacaoobjetos/javacore/introducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,11 @@
+package academy.prigarcia.orientacaoobjetos.javacore.introducaometodos.test;
+
+import academy.prigarcia.orientacaoobjetos.javacore.introducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(10, 20);
+
+    }
+}

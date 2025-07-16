e80a5d076a44aff361d6fa5e7e8571e0d8990e7e
prigarciaa <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>
2025-07-16 19:09:28 -0300

    Método retorno02

--- src/academy/prigarcia/orientacaoobjetos/javacore/introducaometodos/dominio/Calculadora.java
+++ src/academy/prigarcia/orientacaoobjetos/javacore/introducaometodos/dominio/Calculadora.java
@@ -9,11 +9,33 @@ public class Calculadora {
         System.out.println(21 - 2);
 
     }
+
     public void multiplicaDoisNumeros(int num1, int num2) {
         System.out.println(num1 * num2);
     }
 
     public double divideDoisNumeros(double num1, double num2) {
-        return num1/num2;
+        if (num2 == 0) {
+            return 0;
+        }
+        return num1 / num2;
+    }
+
+    public double divideDoisNumeros2(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        }
+        return 0;
+    }
+
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("Não existe divisão por zero");
+            return;
+        }
+        System.out.println(num1 / num2);
     }
 }
+
+
+

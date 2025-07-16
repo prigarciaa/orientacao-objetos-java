625af902bfe7268320d5ba098269c2667ab39118
prigarciaa <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>
2025-07-16 15:33:24 -0300

    Métodos

---
+++ src/academy/prigarcia/orientacaoobjetos/javacore/introducaometodos/dominio/Calculadora.java
@@ -0,0 +1,12 @@
+package academy.prigarcia.orientacaoobjetos.javacore.introducaometodos.dominio;
+
+public class Calculadora {
+    public void somaDoisNumeros() {
+        System.out.println(10 + 10);
+    }
+
+    public void subtraiDoisNumeros() {
+        System.out.println(21 - 2);
+
+    }
+}
---
+++ src/academy/prigarcia/orientacaoobjetos/javacore/introducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,12 @@
+package academy.prigarcia.orientacaoobjetos.javacore.introducaometodos.test;
+
+import academy.prigarcia.orientacaoobjetos.javacore.introducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        System.out.println("Finalizando CalculadoraTest01");
+        calculadora.subtraiDoisNumeros();
+    }
+}
